876358c83cd7b7195015fc2cbdf1548d49f832c0
adhanif <ad.hanif@outlook.com>
2026-07-30 22:42:49 +0300

    exercise 10 completed

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -15,7 +15,14 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  const current = await redis.get('added_todos');
+  await redis.set('added_todos', Number(current || 0) + 1);
   res.send(todo);
+});
+
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.get('added_todos');
+  res.send({ added_todos: Number(addedTodos || 0) });
 });
 
 const singleRouter = express.Router();
@@ -45,6 +52,18 @@ singleRouter.put('/', async (req, res) => {
   req.todo.done = req.body.done;
   await req.todo.save();
   res.send(req.todo);
+});
+
+/* POST todo. */
+singleRouter.put('/', async (req, res) => {
+  req.todo.text = req.body.text;
+  req.todo.done = req.body.done;
+  await req.todo.save();
+  res.send(req.todo);
+
+  const current = await redis.get('added_todos');
+
+  await redis.set('added_todos', Number(current || 0) + 1);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter);
